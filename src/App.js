20001d03a42ac2a7b8d1b589a99a3dfc0421fcdb
Grace Teng <pelicularities@users.noreply.github.com>
2021-02-08 11:18:20 +0800

    Add API call to App component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const [weatherData, setWeatherData] = useState({});
+
+  const baseUrl = "http://api.openweathermap.org/data/2.5/";
+  const endpoint = "weather";
+  const city = "Singapore";
+
+  const [queryUrl, setQueryUrl] = useState(
+    `${baseUrl}${endpoint}?q=${city}&appid=${process.env.REACT_APP_OWM_API_KEY}`
   );
+
+  useEffect(() => {
+    fetch(queryUrl)
+      .then((response) => response.json())
+      .then((json) => {
+        setWeatherData(json);
+      })
+      .catch((err) => console.log(err));
+  }, [queryUrl]);
+
+  console.log(weatherData);
+
+  return <div className="App">{weatherData.main.temp}</div>;
 }
 
 export default App;
