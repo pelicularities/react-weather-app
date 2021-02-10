fffcbb5ec4e64fe334cdf158938a4d2dc8bd6de4
Grace Teng <pelicularities@users.noreply.github.com>
2021-02-10 16:10:19 +0800

    Display Loader component on first page load

--- src/components/LoaderApp.js
+++ src/components/LoaderApp.js
@@ -1,8 +1,15 @@
 import React from "react";
 import "../stylesheets/LoaderApp.css";
+import Loader from "react-loader-spinner";
 
 function LoaderApp({ loading }) {
-  return loading && <div className="loading">Loading...</div>;
+  return (
+    loading && (
+      <div className="loading">
+        <Loader type="TailSpin" color="#888888" height={200} width={200} />
+      </div>
+    )
+  );
 }
 
 export default LoaderApp;
